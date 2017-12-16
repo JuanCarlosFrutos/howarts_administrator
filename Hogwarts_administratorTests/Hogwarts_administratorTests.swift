c10ef74f50d8a5ab2f0e2e88cbdeb99087dfdd5f
Juan Carlos Frutos Hernández <juancarlos3frutos@usal.es>
2017-12-16 16:20:11 +0100

    Initial Commit

---
+++ Hogwarts_administratorTests/Hogwarts_administratorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Hogwarts_administratorTests.swift
+//  Hogwarts_administratorTests
+//
+//  Created by Juan Carlos Frutos Hernández on 16/12/2017.
+//  Copyright © 2017 Juan Carlos Frutos Hernández. All rights reserved.
+//
+
+import XCTest
+@testable import Hogwarts_administrator
+
+class Hogwarts_administratorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
