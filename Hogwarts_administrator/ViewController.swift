67b108c8d3ffdd5f50c599ec9ae6a16468ae7da7
Juan Carlos Frutos Hernández <juancarlos3frutos@usal.es>
2017-12-16 17:33:05 +0100

    First view

--- Hogwarts_administrator/ViewController.swift
+++ Hogwarts_administrator/ViewController.swift
@@ -10,15 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var surname: UITextField!
+    @IBOutlet weak var house: UITextField!
+    @IBOutlet weak var image: UIImageView!
+    
+    var myImage = UIImage(contentsOfFile:"/Users/jcarlos/Documents/desarrollo/Hogwarts_administrator/images/maria.jpg" )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        name.text = "Juan Carlos"
+        surname.text = "Frutos"
+        house.text = "La mia"
+        image.image = myImage
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
